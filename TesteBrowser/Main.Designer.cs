3cf56648ac18a408f13ca57368816bc3c088d804
Muud12 <gu.bat98@gmail.com>
2015-10-02 00:44:23 -0300

    Guitar Flash Bot
    Guitar Flash Bot

---
+++ TesteBrowser/Main.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace TesteBrowser
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.configXYToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.geckoWebBrowser1 = new Skybound.Gecko.GeckoWebBrowser();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(596, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(232, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(514, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.stopToolStripMenuItem,
+            this.configXYToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(765, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "msComando";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // configXYToolStripMenuItem
+            // 
+            this.configXYToolStripMenuItem.Name = "configXYToolStripMenuItem";
+            this.configXYToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.configXYToolStripMenuItem.Text = "Config X & Y ";
+            this.configXYToolStripMenuItem.Click += new System.EventHandler(this.configXYToolStripMenuItem_Click);
+            // 
+            // geckoWebBrowser1
+            // 
+            this.geckoWebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.geckoWebBrowser1.Location = new System.Drawing.Point(0, 24);
+            this.geckoWebBrowser1.Name = "geckoWebBrowser1";
+            this.geckoWebBrowser1.Size = new System.Drawing.Size(765, 422);
+            this.geckoWebBrowser1.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(765, 422);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(765, 446);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.geckoWebBrowser1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Main";
+            this.Text = " : Bot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem configXYToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private Skybound.Gecko.GeckoWebBrowser geckoWebBrowser1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ TesteBrowser/Main.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Threading;
+using Skybound.Gecko;
+
+
+namespace TesteBrowser
+{
+    public partial class Main : Form
+    {
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+        public static extern int BitBlt(IntPtr hDC, int x, int y, int nWidth, int nHeight, IntPtr hSrcDC, int xSrc, int ySrc, int dwRop);
+
+        string color = "";
+        Thread A,S,J,K,L,m;
+        
+        int mX, mY, x, y, i, xA, xS, xJ, xK, xL, allY, contKey = 0;
+        
+        SaveFileDialog saveScreenshot = new SaveFileDialog();
+        private static Bitmap bmpScreenshot;
+        private static Graphics gfxScreenshot;
+        ManualResetEvent _shutdownEvent = new ManualResetEvent(false);
+        ManualResetEvent _pauseEvent = new ManualResetEvent(true);
+        delegate void SetTextCallback(string text);
+        
+
+        public Main()
+        {
+            InitializeComponent();
+            Skybound.Gecko.Xpcom.Initialize("C:\\Users\\gubat_000\\Documents\\Visual Studio 2013\\Projects\\TesteBrowser\\TesteBrowser\\bin\\Debug\\xulrunner");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            // Screenshot
+            this.Hide();
+            // Deixa o bitmap do tamanho da tela
+            bmpScreenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
+            // Cria o objeto a partir do bitmap
+            gfxScreenshot = Graphics.FromImage(bmpScreenshot);
+            gfxScreenshot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, Screen.PrimaryScreen.Bounds.Size, CopyPixelOperation.SourceCopy);
+            // Salva a screen
+            bmpScreenshot.Save("C:\\Users\\gubat_000\\Documents\\Visual Studio 2013\\Projects\\TesteBrowser\\TesteBrowser\\bin\\Debug\\teste.png", ImageFormat.Png);
+
+            this.Show();
+            label1.Text = color;
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            x = 466;
+            xA = 608;
+            xS = 698;
+            xJ = 788;
+            xK = 878;
+            xL = 968;
+            y = 649;
+            A = new Thread(updateA);
+            S = new Thread(updateS);
+            J = new Thread(updateJ);
+            K = new Thread(updateK);
+            L = new Thread(updateL);
+
+            m = new Thread(mousePos);
+            geckoWebBrowser1.Navigate("http:\\guitarflash.com");
+
+        }
+
+        private void updateA()
+        {
+           
+                while (true)
+                {
+                    _pauseEvent.WaitOne(Timeout.Infinite);
+
+                    if (_shutdownEvent.WaitOne(0))
+                        break;
+                    checkKey("A", xA,"ff009800");
+                   
+                }
+          
+        }
+
+        private void updateS()
+        {
+
+            while (true)
+            {
+                _pauseEvent.WaitOne(Timeout.Infinite);
+
+                if (_shutdownEvent.WaitOne(0))
+                    break;
+
+               checkKey("S", xS, "ffff0000");
+                
+            }
+
+        }
+
+        private void updateJ()
+        {
+
+            while (true)
+            {
+                _pauseEvent.WaitOne(Timeout.Infinite);
+
+                if (_shutdownEvent.WaitOne(0))
+                    break;
+
+                checkKey("J", xJ, "fff4f402");
+                
+            }
+
+        }
+
+        private void updateK()
+        {
+
+            while (true)
+            {
+                _pauseEvent.WaitOne(Timeout.Infinite);
+
+                if (_shutdownEvent.WaitOne(0))
+                    break;
+
+                checkKey("K", xK, "0098ff");
+                
+            }
+
+        }
+
+
+        private void updateL()
+        {
+
+            while (true)
+            {
+                _pauseEvent.WaitOne(Timeout.Infinite);
+
+                if (_shutdownEvent.WaitOne(0))
+                    break;
+
+                checkKey("L", xL, "ffff6500");
+            }
+
+        }
+
+        private void checkKey(string key, int xCord, string cor)
+        {
+            
+            using (Graphics gsrc = Graphics.FromHwnd(IntPtr.Zero))
+            {
+     Bitmap screenCopy = new Bitmap(1, 1);
+            using (Graphics gdest = Graphics.FromImage(screenCopy))
+            {
+                IntPtr hSrcDC = gsrc.GetHdc();
+                IntPtr hDC = gdest.GetHdc();
+                int retval = BitBlt(hDC, 0, 0, 1, 1, hSrcDC, xCord, y, (int)CopyPixelOperation.SourceCopy);
+
+                gdest.ReleaseHdc();
+                gsrc.ReleaseHdc();
+
+            }
+
+            Color c = Color.FromArgb(screenCopy.GetPixel(0, 0).ToArgb());
+            label1.ForeColor = c;
+            color = c.Name;
+            
+            if (c.Name == cor)
+            {
+                
+                SendKeys.SendWait("{"+key+"}");
+            }
+           }
+        }
+        private void mousePos()
+        {
+            while (true) 
+           // while (MousePosition.IsEmpty != true && m.ThreadState == System.Threading.ThreadState.Running)
+            {
+                _pauseEvent.WaitOne(Timeout.Infinite);
+
+                if (_shutdownEvent.WaitOne(0))
+                    break;
+
+                mX = Cursor.Position.X;
+                mY = Cursor.Position.Y;
+                setText("X = " + mX.ToString() + " Y = " + mY.ToString());
+                
+            }
+            
+
+        }
+
+        
+
+        private void setText(string text)
+        {
+            try
+            {
+                if (this.label2.InvokeRequired)
+                {
+                    SetTextCallback d = new SetTextCallback(setText);
+                    this.Invoke(d, new object[] { text });
+                }
+                else
+                {
+                    this.label2.Text = text;
+                }
+            }
+
+            catch
+            {
+                MessageBox.Show("Parado com Sucesso!");
+            }
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (i == 0)
+            {
+                m.Start();
+                A.Start();
+                S.Start();
+                J.Start();
+                K.Start();
+                L.Start();
+            }
+            else 
+            {
+                Resume();
+            }
+            
+            i++;
+
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Pause();
+            
+        }
+
+        public void Pause()
+        {
+            _pauseEvent.Reset();
+        }
+
+        public void Resume()
+        {
+            _pauseEvent.Set();
+        }
+
+        private void configXYToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = true;
+            pictureBox1.Image = takeScreen();
+        }
+
+        private Bitmap takeScreen()
+        {
+            // Screenshot
+            this.Hide();
+            // Deixa o bitmap do tamanho da tela
+            bmpScreenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
+            // Cria o objeto a partir do bitmap
+            gfxScreenshot = Graphics.FromImage(bmpScreenshot);
+            gfxScreenshot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, Screen.PrimaryScreen.Bounds.Size, CopyPixelOperation.SourceCopy);
+            // Salva a screen
+            bmpScreenshot.Save("C:\\Users\\gubat_000\\Documents\\Visual Studio 2013\\Projects\\TesteBrowser\\TesteBrowser\\bin\\Debug\\teste.png", ImageFormat.Png);
+
+            this.Show();
+            label1.Text = color;
+
+            return bmpScreenshot;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(xA.ToString());
+            if (contKey == 0)
+            {
+                allY = Cursor.Position.Y;
+                xA = Cursor.Position.X;
+                i++;
+            }
+            if (contKey == 1)
+            {
+                xS = Cursor.Position.X;
+                i++;
+            }
+
+            if (contKey == 2)
+            {
+                xJ = Cursor.Position.X;
+                i++;
+            }
+
+            if (contKey == 3)
+            {
+                xK = Cursor.Position.X;
+                i++;
+            }
+
+            if (contKey == 4)
+            {
+                xL = Cursor.Position.X;
+                i++;
+            }
+            if(contKey == 5)
+            {
+                MessageBox.Show("Teste");
+            }
+        }
+        
+    }
+
+}
+       
